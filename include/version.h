c997527bf8f938de71345fcd7571c0fd6ce9b111
tim <tim@omarov.net>
2022-07-29 11:01:08 +0300

    master

---
+++ include/version.h
@@ -0,0 +1,9 @@
+
+    // AUTO GENERATED FILE, DO NOT EDIT
+    #ifndef VERSION
+        #define VERSION "0.1.343"
+    #endif
+    #ifndef BUILD_TIMESTAMP
+        #define BUILD_TIMESTAMP "2022-07-29 10:57"
+    #endif
+    
